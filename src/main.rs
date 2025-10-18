abad9a86aa8fe75d7519d3cf83efddb281e5c8cf
catfinated <20151825+catfinated@users.noreply.github.com>
2025-10-18 14:15:56 -0700

    refactor to better encapsulate md2 component state

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use bevy::{
 };
 use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiPrimaryContextPass, PrimaryEguiContext};
 
-use md2_bevy::md2::MD2;
+use md2_bevy::md2::{Animation, Skin, MD2};
 use rand::prelude::*;
 use std::path::Path;
 
@@ -21,7 +21,8 @@ struct MD2Component {
 }
 
 impl MD2Component {
-    fn new(md2: MD2) -> Self {
+    fn load(fpath: &Path) -> Self {
+        let md2 = MD2::load(fpath);
         let skin_idx = rand::rng().random_range(0..md2.skins.len());
         let anim_idx = rand::rng().random_range(0..md2.animations.len());
         let materials: Vec<Option<Handle<StandardMaterial>>> = vec![None; md2.skins.len()];
@@ -36,16 +37,22 @@ impl MD2Component {
         }
     }
 
+    // Skins
+    fn skins(&self) -> &[Skin] {
+        &self.md2.skins
+    }
+
     fn skin_name(&self) -> &str {
         &self.md2.skins[self.skin_idx].name
     }
 
-    fn num_anim_frames(&self) -> usize {
-        self.md2.animations[self.anim_idx].key_frames.len()
-    }
-
-    fn anim_name(&self) -> &str {
-        &self.md2.animations[self.anim_idx].name
+    fn next_skin(
+        &mut self,
+        asset_server: &Res<AssetServer>,
+        materials: &mut ResMut<Assets<StandardMaterial>>,
+    ) -> MeshMaterial3d<StandardMaterial> {
+        let new_idx = (self.skin_idx + 1) % self.md2.skins.len();
+        self.set_skin_idx(new_idx, asset_server, materials)
     }
 
     fn set_skin_idx(
@@ -71,11 +78,88 @@ impl MD2Component {
         MeshMaterial3d(self.materials[idx].as_ref().unwrap().clone())
     }
 
+    // Animations
+    fn animations(&self) -> &[Animation] {
+        &self.md2.animations
+    }
+
+    fn num_anim_frames(&self) -> usize {
+        self.md2.animations[self.anim_idx].key_frames.len()
+    }
+
+    fn next_anim(&mut self) {
+        let next = (self.anim_idx + 1) % self.md2.animations.len();
+        self.set_anim_idx(next);
+    }
+
+    fn anim_name(&self) -> &str {
+        &self.md2.animations[self.anim_idx].name
+    }
+
     fn set_anim_idx(&mut self, idx: usize) {
         self.anim_idx = idx;
         self.curr_frame = 0;
         self.interp = 0.0;
     }
+
+    fn animate(&mut self, delta: f32) -> Vec<Vec3> {
+        let mut interp = self.interp + (8.0f32 * delta);
+        let mut current = self.curr_frame;
+        let mut next = (current + 1) % self.num_anim_frames();
+
+        if interp >= 1.0f32 {
+            current = next;
+            next = (current + 1) % self.num_anim_frames();
+            interp = 0.0f32;
+        }
+        self.interp = interp;
+        self.curr_frame = current;
+
+        let curr_v = &self.md2.animations[self.anim_idx].key_frames[current];
+        let next_v = &self.md2.animations[self.anim_idx].key_frames[next];
+        let mut v = Vec::new();
+        v.reserve(curr_v.len());
+
+        for i in 0..curr_v.len() {
+            v.push(curr_v[i].lerp(next_v[i], interp));
+        }
+
+        v
+    }
+
+    fn create_mesh(&self) -> Mesh {
+        Mesh::new(
+            PrimitiveTopology::TriangleList,
+            RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+        )
+        .with_inserted_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            self.md2.animations[self.anim_idx].key_frames[self.curr_frame].clone(),
+        )
+        .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, self.md2.texcoords.clone())
+    }
+}
+
+fn spawn_md2(
+    path: &Path,
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    meshes: &mut ResMut<Assets<Mesh>>,
+) {
+    let mut md2 = MD2Component::load(path);
+    let mat3d = md2.set_skin_idx(md2.skin_idx, asset_server, materials);
+    let mesh_handle: Handle<Mesh> = meshes.add(md2.create_mesh());
+    let scale = 1.0_f32 / 32.0_f32;
+    let neg90 = f32::to_radians(-90.0);
+
+    commands.spawn((
+        Mesh3d(mesh_handle),
+        mat3d,
+        Transform::from_rotation(Quat::from_euler(EulerRot::XYZEx, 0.0, neg90, 0.0))
+            .with_scale(Vec3::splat(scale)),
+        md2,
+    ));
 }
 
 fn main() {
@@ -83,9 +167,8 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin::default())
         .add_systems(Startup, setup)
-        .add_systems(Update, keyboard_input_system)
-        .add_systems(Update, animation_system)
-        .add_systems(EguiPrimaryContextPass, ui_example_system)
+        .add_systems(Update, (keyboard_input_system, animation_system))
+        .add_systems(EguiPrimaryContextPass, ui_system)
         .run();
 }
 
@@ -95,20 +178,13 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    let md2 = MD2::load(&Path::new("assets/models/ogro/tris.md2"));
-    let mut component = MD2Component::new(md2);
-    let mat3d = component.set_skin_idx(component.skin_idx, &asset_server, &mut materials);
-    let mesh_handle: Handle<Mesh> = meshes.add(create_mesh(&component));
-    let scale = 1.0_f32 / 32.0_f32;
-    let neg90 = f32::to_radians(-90.0);
-
-    commands.spawn((
-        Mesh3d(mesh_handle),
-        mat3d,
-        Transform::from_rotation(Quat::from_euler(EulerRot::XYZEx, 0.0, neg90, 0.0))
-            .with_scale(Vec3::splat(scale)),
-        component,
-    ));
+    spawn_md2(
+        &Path::new("assets/models/ogro/tris.md2"),
+        &mut commands,
+        &asset_server,
+        &mut materials,
+        &mut meshes,
+    );
 
     // Transform for the camera and lighting, looking at (0,0,0) (the position of the mesh).
     let camera_transform = Transform::from_xyz(1.0, 1.0, 3.0).looking_at(Vec3::ZERO, Vec3::Y);
@@ -129,18 +205,6 @@ fn setup(
     ));
 }
 
-fn create_mesh(md2c: &MD2Component) -> Mesh {
-    Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
-    )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        md2c.md2.animations[md2c.anim_idx].key_frames[0].clone(),
-    )
-    .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, md2c.md2.texcoords.clone())
-}
-
 fn keyboard_input_system(
     mut commands: Commands,
     keyboard_input: Res<ButtonInput<KeyCode>>,
@@ -149,16 +213,14 @@ fn keyboard_input_system(
     mut query: Query<(Entity, &mut MD2Component)>,
 ) {
     if keyboard_input.just_pressed(KeyCode::KeyS) {
-        let (entity, mut md2c) = query.single_mut().unwrap();
-        let new_idx = (md2c.skin_idx + 1) % md2c.md2.skins.len();
-        let new_mat = md2c.set_skin_idx(new_idx, &asset_server, &mut materials);
+        let (entity, mut md2) = query.single_mut().unwrap();
+        let new_mat = md2.next_skin(&asset_server, &mut materials);
         commands.entity(entity).insert(new_mat);
     }
 
     if keyboard_input.just_pressed(KeyCode::KeyA) {
-        let (_, mut md2c) = query.single_mut().unwrap();
-        let next = (md2c.anim_idx + 1) % md2c.md2.animations.len();
-        md2c.set_anim_idx(next);
+        let (_, mut md2) = query.single_mut().unwrap();
+        md2.next_anim();
     }
 }
 
@@ -167,34 +229,13 @@ fn animation_system(
     mut meshes: ResMut<Assets<Mesh>>,
     mut query: Query<(&Mesh3d, &mut MD2Component)>,
 ) {
-    let (mesh, mut md2c) = query.single_mut().unwrap();
-    let mut interp = md2c.interp + (8.0f32 * time.delta_secs());
-    let mut current = md2c.curr_frame;
-    let mut next = (current + 1) % md2c.num_anim_frames();
-
-    if interp >= 1.0f32 {
-        current = next;
-        next = (current + 1) % md2c.num_anim_frames();
-        interp = 0.0f32;
-    }
-    md2c.interp = interp;
-    md2c.curr_frame = current;
-
-    let curr_v = &md2c.md2.animations[md2c.anim_idx].key_frames[current];
-    let next_v = &md2c.md2.animations[md2c.anim_idx].key_frames[next];
-    let mut v = Vec::new();
-    v.reserve(curr_v.len());
-
-    for i in 0..curr_v.len() {
-        v.push(curr_v[i].lerp(next_v[i], interp));
-    }
-
+    let (mesh, mut md2) = query.single_mut().unwrap();
+    let vertices = md2.animate(time.delta_secs());
     let m = meshes.get_mut(mesh.id()).unwrap();
-
-    m.insert_attribute(Mesh::ATTRIBUTE_POSITION, v);
+    m.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
 }
 
-fn ui_example_system(
+fn ui_system(
     mut contexts: EguiContexts,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -209,7 +250,7 @@ fn ui_example_system(
         egui::ComboBox::from_label("[s]kin")
             .selected_text(md2.skin_name())
             .show_ui(ui, |ui| {
-                for (idx, skin) in md2.md2.skins.iter().enumerate() {
+                for (idx, skin) in md2.skins().iter().enumerate() {
                     ui.selectable_value(&mut curr_skin, idx, &skin.name);
                 }
             });
@@ -222,7 +263,7 @@ fn ui_example_system(
         egui::ComboBox::from_label("[a]nim")
             .selected_text(md2.anim_name())
             .show_ui(ui, |ui| {
-                for (idx, anim) in md2.md2.animations.iter().enumerate() {
+                for (idx, anim) in md2.animations().iter().enumerate() {
                     ui.selectable_value(&mut curr_anim, idx, &anim.name);
                 }
             });
